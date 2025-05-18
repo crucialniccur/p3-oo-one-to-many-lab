271c99f09367c259139f656537f57c7300841de5
Mati Joseph <crucialniccur@gmail.com>
2025-05-18 15:38:02 +0300

    validate that pet_type is in one of PET_TYPES in the Pet init methods and if not raise an Exception

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -3,8 +3,11 @@ class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
 
     def __init__(self, name, pet_type):
-        self.name = name
-        self.pet_type = pet_type
+        if pet_type in Pet.PET_TYPES:
+            self.name = name
+            self.pet_type = pet_type
+        else:
+            raise Exception('Type must be among  PET_TYPES!')
 
 
 class Owner:
