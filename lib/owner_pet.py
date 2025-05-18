8ca2ed84433664ba7797c56fc687a5863b278697
Mati Joseph <crucialniccur@gmail.com>
2025-05-18 15:45:33 +0300

    finish tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,19 +1,33 @@
-class Pet:
-
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all = []
-
-    def __init__(self, name, pet_type):
-        if pet_type in Pet.PET_TYPES:
-            self.name = name
-            self.pet_type = pet_type
-        else:
-            raise Exception('Type must be among  PET_TYPES!')
-
-
 class Owner:
     def __init__(self, name):
         self.name = name
 
     def pets(self):
-        return Pet.all
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Can only add instances of Pet.")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if not isinstance(name, str):
+            raise Exception("Name must be a string.")
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Type must be among PET_TYPES!")
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of Owner or None.")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        Pet.all.append(self)
