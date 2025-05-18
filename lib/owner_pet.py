03578e705c4003c0f6062bf27143f3dfd5f25a77
Mati Joseph <crucialniccur@gmail.com>
2025-05-18 15:29:30 +0300

    define a class variable, PET_TYPES in the Pet class

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,4 +1,7 @@
 class Pet:
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
     def __init__(self, name, pet_type):
         self.name = name
         self.pet_type = pet_type
